302a4d5f286e150120956f70be92494495b9f1fc
Chad Simpson <drshockalot@hotmail.com>
2016-05-20 20:17:56 +0100

    heroku push

--- db/database.js
+++ db/database.js
@@ -1,6 +1,6 @@
 var config = require('./knexfile');
 
-var knex = require('knex')(config[development]);
+var knex = require('knex')(config['development']);
 
 var bookshelf = require('bookshelf')(knex);
 bookshelf.plugin('registry');
