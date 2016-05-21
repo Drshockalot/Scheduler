2055a97123cb633416e11a7c6cf4e26cac4f61d9
Chad Simpson <drshockalot@hotmail.com>
2016-05-21 21:47:24 +0100

    heroku push

--- app/components/App.js
+++ app/components/App.js
@@ -6,7 +6,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Navbar history={this.props.history} url={this.props.location} />
+        <Navbar history={this.props.history}/>
         {this.props.children}
         <Footer />
       </div>
