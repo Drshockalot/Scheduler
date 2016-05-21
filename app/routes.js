4b33d37c4bdbb735a2625a85106e53e4ba143931
Chad Simpson <drshockalot@hotmail.com>
2016-05-21 23:21:38 +0100

    heroku push

--- app/routes.js
+++ app/routes.js
@@ -4,11 +4,12 @@ import App from './components/App';
 import Home from './components/Home';
 import Admin from './components/admin/Admin';
 import RosterManagement from './components/admin/RosterManagement';
+import Register from './components/Register';
 
 export default (
   <Route component={App}>
     <Route path='/' component={Home} />
-    <Route path='/register' component={Register} />
     <Route path='/admin' component={Admin} />
+    <Route path='/admin/roster' component={RosterManagement} />
   </Route>
 );
