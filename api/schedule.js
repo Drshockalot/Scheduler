21add87cbeb4daac1cccabb3e83f769b0f185b0d
Chad Simpson <drshockalot@hotmail.com>
2016-06-22 21:33:54 +0100

    heroku push

--- api/schedule.js
+++ api/schedule.js
@@ -10,7 +10,7 @@ var Roster = require('./../db/postgres/roster');
 
 router.get('/', function(req, res, next) {
   Schedule.forge()
-          .fetchAll({'withRelated': ['schedule_bosses', 'schedule_bosses.characters', 'schedule_bosses.boss', 'schedule_bosses.raid', 'roster']})
+          .fetchAll({'withRelated': ['schedule_bosses', 'schedule_bosses.characters', 'schedule_bosses.boss', 'schedule_bosses.raid', 'roster', 'roster.characters']})
           .then(function(schedules) {
             Character.forge()
                     .fetchAll()
@@ -21,7 +21,7 @@ router.get('/', function(req, res, next) {
                                 RaidWeek.forge()
                                         .fetchAll()
                                         .then(function(raidweeks) {
-                                          Roster.forge()
+                                          Roster.forge({'withRelated': ['characters']})
                                                 .fetchAll()
                                                 .then(function(rosters) {
                                                   res.json({error: false, data: {message: "data found", schedules: schedules.toJSON(),
@@ -58,7 +58,7 @@ router.post('/admin', function(req, res, next) {
           .save()
           .then(function(schedule) {
             Schedule.forge()
-                    .fetchAll({'withRelated': ['schedule_bosses', 'schedule_bosses.characters', 'schedule_bosses.boss', 'schedule_bosses.raid']})
+                    .fetchAll({'withRelated': ['schedule_bosses', 'schedule_bosses.characters', 'schedule_bosses.boss', 'schedule_bosses.raid', 'roster', 'roster.characters']})
                     .then(function(schedules) {
                       res.json({error: false, data: {message: "Schedule added", schedules: schedules.toJSON()}});
                     })
@@ -81,7 +81,7 @@ router.post('/admin/boss', function(req, res, next) {
                .save()
                .then(function(schedule_boss) {
                  Schedule.forge()
-                         .fetchAll({'withRelated': ['schedule_bosses', 'schedule_bosses.characters', 'schedule_bosses.boss', 'schedule_bosses.raid']})
+                         .fetchAll({'withRelated': ['schedule_bosses', 'schedule_bosses.characters', 'schedule_bosses.boss', 'schedule_bosses.raid', 'roster', 'roster.characters']})
                          .then(function(schedules) {
                            res.json({error: false, data: {message: "Schedule boss added", schedules: schedules.toJSON()}});
                          })
