1be01ceddea7f84cd6dc5f84424ab3c01ca014f8
Chad Simpson <drshockalot@hotmail.com>
2016-07-09 19:03:16 +0100

    heroku push

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -4676,15 +4676,15 @@ var RaidManagement = function (_React$Component) {
                   ),
                   _react2.default.createElement(
                     'td',
-                    { className: 'col-md-5' },
+                    { style: 'word-wrap: break-word', className: 'col-md-5' },
                     _react2.default.createElement(
                       'div',
-                      { className: 'col-md-6' },
+                      { style: 'word-wrap: break-word', className: 'col-md-6' },
                       boss.public_note
                     ),
                     _react2.default.createElement(
                       'div',
-                      { className: 'col-md-6' },
+                      { style: 'word-wrap: break-word', className: 'col-md-6' },
                       boss.officer_note
                     )
                   ),
@@ -5120,7 +5120,7 @@ var RaidManagement = function (_React$Component) {
               ),
               _react2.default.createElement(
                 'table',
-                { className: 'table' },
+                { style: 'table-layout: fixed', className: 'table' },
                 _react2.default.createElement(
                   'tbody',
                   null,
