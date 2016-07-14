0f4296b7dea042fb02bd4c1ea0fbec63e01b2519
Chad Simpson <drshockalot@hotmail.com>
2016-07-14 22:08:37 +0100

    heroku push

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1901,7 +1901,9 @@ var Home = function (_React$Component) {
                             _react2.default.createElement(
                               'li',
                               null,
-                              'Clicking on a character name show shows that user\'s availability for the week'
+                              'Clicking on a character name show shows that ',
+                              "user's",
+                              ' availability for the week'
                             )
                           )
                         )
